30703908d8a80e3d6b664047306bb7d178ed4fd7
EdgardoAVS <edgardoovargass@gmail.com>
2021-08-10 15:46:54 -0500

    Taler Analisis salarial terminado

---
+++ tallerAnalisisSalarial/salarios.js
@@ -0,0 +1,55 @@
+const colombia = []
+
+colombia.push({
+  name: "Juan",
+  salary: 1000,
+});
+colombia.push({
+  name: "Edgardo",
+  salary: 10000,
+});
+colombia.push({
+  name: "Valentina",
+  salary: 5000,
+});
+colombia.push({
+  name: "Andres",
+  salary: 4000,
+});
+colombia.push({
+  name: "Luis",
+  salary: 400,
+});
+colombia.push({
+  name: "Pedro",
+  salary: 300,
+});
+colombia.push({
+  name: "Fabian",
+  salary: 7000,
+});
+colombia.push({
+  name: "Marvin",
+  salary: 8000,
+});
+colombia.push({
+  name: "Erik",
+  salary: 5000,
+});
+colombia.push({
+  name: "Thomas",
+  salary: 500,
+});
+colombia.push({
+  name: "Mark",
+  salary: 900,
+});
+colombia.push({
+  name: "Lorenzo",
+  salary: 1400,
+});
+colombia.push({
+  name: "Gustavo",
+  salary: 1470,
+});
+
